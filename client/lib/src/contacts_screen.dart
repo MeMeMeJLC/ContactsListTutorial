8b84c35a02e71c5b1ce10ebcb7f3683c3b922975
MeMeMeJLC <jeremy.cook.llb@live.com>
2021-08-31 21:25:30 +1200

    error

--- client/lib/src/contacts_screen.dart
+++ client/lib/src/contacts_screen.dart
@@ -42,6 +42,21 @@ class _ContactsScreenState extends State<ContactsScreen> {
         contacts: contacts,
         onAdd: _addContact,
         //onDelete: _deleteContact,
+        
+        /*════════ Exception caught by widgets library ═══════════════════════════════════
+The following assertion was thrown building ContactsListing(dirty):
+setState() or markNeedsBuild() called during build.
+
+This ContactsScreen widget cannot be marked as needing to build because the framework is already in the process of building widgets.  A widget can be marked as needing to be built during the build phase only if one of its ancestors is currently building. This exception is allowed because the framework builds parent widgets before children, which means a dirty descendant will always be built. Otherwise, the framework might not visit this widget during this build phase.
+The widget on which setState() or markNeedsBuild() was called was: ContactsScreen
+    state: _ContactsScreenState#8eb96
+The widget which was currently being built when the offending call was made was: ContactsListing
+    dirty
+The relevant error-causing widget was
+ContactsListing
+package:contacts_app_client/src/contacts_screen.dart:41
+When the exception was thrown, this was the stack
+*/
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
